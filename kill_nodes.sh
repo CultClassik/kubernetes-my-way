f1d8dc7319d7a182cd3e085d295f1e3c2b669049
Chris Diehl <cultclassik@gmail.com>
2020-08-09 14:57:41 -0500

    Update kill node script

--- kill_nodes.sh
+++ kill_nodes.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 vboxmanage controlvm kn1 poweroff
-vboxmanage controlvm kn2 poweroff
-vboxmanage controlvm kn3 poweroff
 vboxmanage unregistervm kn1 --delete
+vboxmanage controlvm kn2 poweroff
 vboxmanage unregistervm kn2 --delete
+vboxmanage controlvm kn3 poweroff
 vboxmanage unregistervm kn3 --delete
-
