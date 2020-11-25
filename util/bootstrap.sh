a5e9ef045cbad372d69fecbed694ca02d9d24eef
Chris Diehl <culktclassik@gmail.com>
2020-11-25 11:50:00 -0600

    Update bootstrap

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 vagrant up
+ansible-galaxy install -r ./ansible/requirements.yml
 ansible-playbook -i ./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ./ansible/main.yml
