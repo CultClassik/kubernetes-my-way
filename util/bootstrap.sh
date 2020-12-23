cbaa91ca61da00d4ab202cf2282863866f253236
Chris Diehl <culktclassik@gmail.com>
2020-12-23 08:21:42 -0600

    fix

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd..
-ansible-galaxy collection install community.kubernetes
+ansible-galaxy collection install kubernetes.core
 ansible-galaxy install -r ./ansible/requirements.yml
 vagrant up
 ansible-playbook -i ./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ./ansible/main.yml
